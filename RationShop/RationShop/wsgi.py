4e039b999ee6b181a71563b9a4591344f6c40de3
JissmonRaju <jissmonraju555@gmail.com>
2025-04-04 00:21:32 +0530

    Deployed

--- RationShop/RationShop/wsgi.py
+++ RationShop/RationShop/wsgi.py
@@ -1,16 +1,16 @@
-"""
-WSGI config for RationShop project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
+# RationShop/wsgi.py
 import os
-
 from django.core.wsgi import get_wsgi_application
+from django.contrib.auth import get_user_model
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RationShop.settings')
-
 application = get_wsgi_application()
+
+# Auto-create superuser
+User = get_user_model()
+username = os.getenv('DJANGO_SUPERUSER_USERNAME')
+email = os.getenv('DJANGO_SUPERUSER_EMAIL')
+password = os.getenv('DJANGO_SUPERUSER_PASSWORD')
+
+if not User.objects.filter(username=username).exists() and all([username, email, password]):
+    User.objects.create_superuser(username, email, password)
